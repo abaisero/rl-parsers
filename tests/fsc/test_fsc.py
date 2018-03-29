6f84affeadf32cb1d668652ed7b7aa5af96b1a4d
abaisero <andrea.baisero@gmail.com>
2018-03-29 17:35:17 -0400

    add FSC parser, test, and optimal .fsc file for tiger problem

---
+++ tests/fsc/test_fsc.py
@@ -0,0 +1,12 @@
+import unittest
+
+import rl_parsers.fsc as dotfsc
+
+
+class FSC_Test(unittest.TestCase):
+    def parse_file(self, fname):
+        with open(fname) as f:
+            dotfsc.parse(f.read())
+
+    def test_parser(self):
+        self.parse_file('tests/fsc/tiger.optimal.fsc')

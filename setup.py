bf0b374cf21eb96f16439e76a26b477798af5cf0
abaisero <andrea.baisero@gmail.com>
2018-11-26 16:04:22 -0500

    move setup dependencies in requirements.txt

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='0.1.0',
     description='rl_parsers - parsers for old and new RL file formats',
     packages=find_packages(),
-    install_requires=['numpy', 'ply'],
+    # install_requires=['numpy', 'ply'],
     # test_suite='tests',
 )

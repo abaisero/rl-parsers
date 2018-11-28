1defa117965d5c009019736f4b94794cc288164e
abaisero <andrea.baisero@gmail.com>
2018-11-27 22:06:01 -0500

    delete requirements, add dependencies in setup file

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     version='0.1.0',
     description='rl_parsers - parsers for old and new RL file formats',
     packages=find_packages(),
-    # install_requires=['numpy', 'ply'],
-    # test_suite='tests',
+    install_requires=['numpy', 'ply'],
 )
